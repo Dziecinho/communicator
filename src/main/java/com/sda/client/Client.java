4514335dbe04bafa8e842a66d01f7d4eebef75b0
dzieciak <pawel_ryta@wp.pl>
2018-04-16 18:42:19 +0200

    working local chat

--- src/main/java/com/sda/client/Client.java
+++ src/main/java/com/sda/client/Client.java
@@ -5,35 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
 
     public static void main(String[] args) throws IOException {
 
+        Scanner scanner = new Scanner(System.in);
+
         //host serwera
         String host = "localhost";
         //port serwera
         int port = 4444;
 
-        //Tworzymy gniazdo do polaczenia z serwerem
-        Socket socket = new Socket(host,port);
-        //BufferedReader odczyt z serwera
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        //PrintWriter - wysylanie do serwera
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        System.out.println("PAWEŁ");
 
-        //Wyslij do serwera
-        printWriter.println("Hello from client!");
-        //Wypchnij z bufora
-        printWriter.flush();
+        while (true) {
+            //Tworzymy gniazdo do polaczenia z serwerem
+            Socket socket = new Socket(host, port);
+            //BufferedReader odczyt z serwera
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //PrintWriter - wysylanie do serwera
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            String message = scanner.nextLine();
 
-        //Odczytaj od serwera
-        String response = reader.readLine();
-        System.out.println("Response: "+response);
-        //Zamknij strumienie
-        printWriter.close();
-        reader.close();
+            //Wyslij do serwera
+            printWriter.println(message);
+            //Wypchnij z bufora
+            printWriter.flush();
+
+            //Odczytaj od serwera
+            String response = reader.readLine();
+            System.out.println(response);
+            //Zamknij strumienie
+            printWriter.close();
+            reader.close();
+        }
 
 
     }
--- src/main/java/com/sda/server/EchoServer.java
+++ src/main/java/com/sda/server/EchoServer.java
@@ -5,10 +5,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
 
     public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
 
         //Port na ktorym startuje serwer
         int port = 4444;
@@ -16,10 +19,11 @@ public class EchoServer {
         long SLEEP_TIME = 5_000L;
         //ServerSocker - gniazdo serwera. Sluzy do przyjmowania klientow TCP
         ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Started server on port " + port);
+        //      System.out.println("Started server on port " + port);
+        System.out.println("JANUSZ");
 
         while (true) {
-            System.out.println("Waiting for client...");
+            //      System.out.println("Waiting for client...");
             //Czekaj na klienta
             Socket clientSocket = serverSocket.accept();
             //BufferedReader odczytuje to co wyslal klient
@@ -29,11 +33,14 @@ public class EchoServer {
 
             //Odczytaj linijke od klienta
             String line = reader.readLine();
-            System.out.println("Received: " + line);
+            System.out.println(line);
             //Uspij na 5 sek. Symulacja obliczen
-            Thread.sleep(SLEEP_TIME);
+            //      Thread.sleep(SLEEP_TIME);
             //Odpowiedz do klienta
-            printWriter.println("Response from server: " + line);
+            //      printWriter.println("Response from server: " + line);
+
+            String message = scanner.nextLine();
+            printWriter.println(message);
             //flush - wypchnij z bufora
             printWriter.flush();
             //Zamknij strumienie
